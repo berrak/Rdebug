d0e74552b35a79d8711d85bf00597181be04ee33
berrak <bkronmailbox-git@yahoo.se>
2022-12-20 09:46:04 +0100

    bump version

--- src/Rdebug.h
+++ src/Rdebug.h
@@ -3,7 +3,6 @@
  * The Arduino library provides a unique form of debugging.
  *
  * Copyright (c) 2021 Ralph Bacon
- * https://github.com/RalphBacon/224-Superior-Serial.print-statements
  *
  * Adapted and modified original code.
  * Copyright (C) 2022 Debinix Team
